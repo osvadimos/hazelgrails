a50a9259da3145204435813127d27f6e39b7d33c
Vadim Osmolovski <osvadimos@gmail.com>
2018-04-06 13:53:42 +0300

    Move config to other place

--- grails-app/services/hazelgrails/HazelService.groovy
+++ grails-app/services/hazelgrails/HazelService.groovy
@@ -1,7 +1,12 @@
 package hazelgrails
 
+import com.hazelcast.aws.AwsDiscoveryStrategyFactory
+import com.hazelcast.config.Config
+import com.hazelcast.config.DiscoveryStrategyConfig
+import com.hazelcast.config.JoinConfig
 import com.hazelcast.core.IExecutorService
 import com.hazelcast.core.Member
+import system.utils.SystemUtils
 
 import java.util.concurrent.Callable
 import java.util.concurrent.Future
@@ -20,9 +25,29 @@ class HazelService {
     static transactional = false
 
     HazelcastInstance instance
+    def grailsApplication
 
     @PostConstruct
     void init() {
+        Config config = new Config()
+        config.getProperties().setProperty("hazelcast.discovery.enabled", "true")
+        JoinConfig joinConfig = config.getNetworkConfig().getJoin()
+        joinConfig.getTcpIpConfig().setEnabled(false)
+        joinConfig.getMulticastConfig().setEnabled(false)
+        joinConfig.getAwsConfig().setEnabled(false)
+        AwsDiscoveryStrategyFactory awsDiscoveryStrategyFactory = new AwsDiscoveryStrategyFactory()
+        Map<String, Comparable> properties = new HashMap<String, Comparable>()
+        properties.put("access-key", grailsApplication.config.awshazel.accessHazelKey.toString())
+        properties.put("secret-key", grailsApplication.config.awshazel.secretHazelKey.toString())
+        properties.put("region", grailsApplication.config.awshazel.region.toString())
+        properties.put("host-header","ec2.amazonaws.com")
+        properties.put("tagKey", "hazelcluster")
+        properties.put("tag-key", "hazelcluster")
+        properties.put("tagValue", "hazelcluster")
+        properties.put("tag-value", "hazelcluster")
+        DiscoveryStrategyConfig discoveryStrategyConfig = new DiscoveryStrategyConfig(awsDiscoveryStrategyFactory, properties)
+        joinConfig.getDiscoveryConfig().addDiscoveryStrategyConfig(discoveryStrategyConfig)
+        config.setInstanceName(SystemUtils.hostname)
         instance = Hazelcast.newHazelcastInstance()
     }
 
